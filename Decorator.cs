b22db0dc771fe2fc9d0e66c04d5db7ce689bbec1
Simoffsky <maks-kirsanov26@mail.ru>
2022-05-30 21:59:27 +1000

    Added decorator pattern

---
+++ Decorator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace CurrentTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamReader reader = new StreamReader("data.txt");
+            string str = reader.ReadLine();
+            Console.WriteLine(str);
+
+        }
+    }
+}
+
